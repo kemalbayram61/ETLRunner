cf3f8c967d0a9a24cd0763bd07633893c9de1887
KemalBayram <kemalbayramag@gmail.com>
2026-03-17 23:33:54 +0300

    spring boot starter web add to pom.xml and add health check service for ETLRunnerBusiness

--- ETLRunnerBus/src/main/java/tr/com/kml/ETLRunnerBus/ETLRunnerBusiness.java
+++ ETLRunnerBus/src/main/java/tr/com/kml/ETLRunnerBus/ETLRunnerBusiness.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ETLRunnerBusiness {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ETLRunnerBusiness.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ETLRunnerBusiness.class, args);
+    }
 
 }
